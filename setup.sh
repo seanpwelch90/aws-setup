596602bfdd6396d0f23983ad2f0c5f1dc0d3462a
Sean Welch <sean.welch@me.com>
2022-01-20 10:42:05 -0800

    first commit

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get update && sudo apt-get upgrade -y
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt install iptables-persistent
